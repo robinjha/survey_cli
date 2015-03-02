89e84100d11144ed17d48337ef2c3f8a8b6b3052
robinjha <robinjha@gmail.com>
2015-03-01 23:17:51 -0500

    adding nested json support

--- src/com/python/survey/cli/filereader.py
+++ src/com/python/survey/cli/filereader.py
@@ -5,43 +5,92 @@ Created on Feb 28, 2015
 '''
 #!/usr/bin/env python
 
-import sys, json
+import sys, json, collections
 from pprint import pprint
-
 class FileReader(object):
     filename = ""
-   
-    def readFile(self, filename):
-        with open(filename,'r') as f:
-            data = f.readlines()
-            
-            for line in data:
-                print line,
+    '''
+    def locateByName(self, e, name):
+        if e.get('name', None) == name:
+            return e
+
+        for child in e.get('children', []):
+            result = locateByName(child, name)
+            if result is not None:
+                return result
+
+        return None
+    
+    def nested_dict_iter(self, nested):
+        for key, value in nested.iteritems():
+            if isinstance(value, collections.Mapping):
+                for inner_key, inner_value in self.nested_dict_iter(value):
+                    yield inner_key, inner_value
+                    print(inner_key, inner_value)
+            else:
+                yield key, value 
+                print(key, value)           
+    '''  
+    def nestedJSON(self, d, responses):
+        for k, v in d.iteritems():
+            if isinstance(v, dict):
+                self.nestedJSON(v, responses)
+            elif(k == 'choices'):
+                for s in v:
+                    print s
+            else:
+                print "{0} : {1}".format(k, v)
+                
+        choice = raw_input("Please enter your choice : ")
+        json.dump({'question': v,'answer':choice}, responses, indent = 4, ensure_ascii = False)
         
+         
     def processSurvey(self, inputfile, outputfile):
         surveyques = open(inputfile, "r")
         responses = open(outputfile, "w")
         while True:
-            text = surveyques.readline()
-            pprint(text)
-            if len(text) == 0:
-                break;
-            
-            ques = raw_input(text)
-            #json_string = json.dumps({'question':text, 'answer': ques}, responses,sort_keys=False,indent=2)
-            #print json_string
-            #print(ques)
-            #responses.write(text)
-            json.dump({'question':text, 'answer': ques}, responses, sort_keys = False, indent = 4, ensure_ascii = False)
-            
-        surveyques.close()
-        responses.close()
+            try:
+                text = json.load(surveyques) 
+                #json_string = json.dumps(text, sort_keys=True, indent = 2)
+                #print(json_string)
+                questions = text['questions']
+                print("No of questions: " , len(questions)) # find out how many questions are in the survey
+                #pprint(questions[2])
+                for item in questions:
+                    self.nestedJSON(item, responses)
+                    #print("#####printing item#######")
+                    #print(item)
+                    
+                    #if(item.has_key('choices')):
+                    #    self.nestedJSON(item, responses)
+                    #else:
+                    #    print(item["question"])
+                    #choice = raw_input("Please enter your choice : ")
+                    #json.dump({'question': item["question"],'answer':choice}, responses, indent = 4, ensure_ascii = False)
+        
+                            
+                    '''   
+                    #self.nested_dict_iter(item)
+                    if "qu" in item:
+                        print("#################")
+                        ques = raw_input("Q: "+item["question"]+" \nA: ")
+                        json.dump({'question':item["question"], 'answer':ques}, responses, indent = 4, ensure_ascii = False)
+                    '''
+                break
+                    
+            except ValueError:
+                continue
+            else:
+                surveyques.close()
+                responses.close()
+        #surveyques.close()
+        #responses.close()
 
     def __init__(self, filename):
         self.filename = sys.argv[0]
         
-ques = FileReader("/Users/robin/Documents/workspace/survey_cli/src/resources/sample_survey.txt")
-inputfile = "/Users/robin/Documents/workspace/survey_cli/src/resources/sample_survey.txt"
+ques = FileReader("/Users/robin/Documents/workspace/survey_cli/src/resources/sample_survey.json")
+inputfile = "/Users/robin/Documents/workspace/survey_cli/src/resources/sample_survey.json"
 outputfile = "/Users/robin/Documents/workspace/survey_cli/src/resources/responses.json"
 ques.processSurvey(inputfile, outputfile)
 #print ques.filename
